63324b7edfba56f81438602a77fe81d82874b298
lggnor1 <161672304+lggnor1@users.noreply.github.com>
2025-06-21 16:03:45 +0900

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,7 +1,3 @@
-window.addEventListener('scroll', handleScroll);
-window.addEventListener('load', handleScroll); // 로딩 시 바로 보여줄 요소 체크
-window.addEventListener("resize", handleScroll); 
-
 // 404-메인페이지 버튼
 document.getElementById("home").addEventListener("click", () => {
     window.location.href = "/";
