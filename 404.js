df6ba8c4f5016d3961390df6778012e5a6651c02
lggnor1 <161672304+lggnor1@users.noreply.github.com>
2025-06-21 15:58:38 +0900

    무조건 메인페이지로

--- 404.js
+++ 404.js
@@ -4,12 +4,5 @@ window.addEventListener("resize", handleScroll);
 
 // 404-메인페이지 버튼
 document.getElementById("home").addEventListener("click", () => {
-
-    const referrer = document.referrer;
-
-    if (referrer && referrer !== location.href) {
-    history.back();
-    } else {
     window.location.href = "/";
-    }
 });
